8a5c6f7c2bd77ca0c8e9c38439c5a79fdf308667
sulifer <937096063@qq.com>
2020-07-03 14:19:51 +0800

    'a'

--- app/worker/worker1/1.php
+++ app/worker/worker1/1.php
@@ -9,7 +9,6 @@ global  $config,$js;
 $waitSeconds = $config["WEBSITE"]["wait"];  //需等待加载的时间，一般加载时间在0-15秒，如果超过15秒，报错。
 $host = $config["WEBSITE"]["host"]; // this is the default
 //这里使用的是chrome浏览器进行测试，需到http://www.seleniumhq.org/download/上下载对应的浏览器测试插件
-//我这里下载的是win32 Google Chrome Driver 2.25版：https://chromedriver.storage.googleapis.com/index.html?path=2.25/
 $capabilities = DesiredCapabilities::chrome();
 $driver = RemoteWebDriver::create($host, $capabilities);
 $driver->get($config["WEBSITE"]["url"]);
--- config/website.php
+++ config/website.php
@@ -1,9 +1,9 @@
 <?php
 return [
     //爬取路由
-  "url"=>"http://www.ssme.sh.gov.cn/public/search!productList.do" ,
+    "url"=>"http://www.ssme.sh.gov.cn/public/search!productList.do" ,
     //chromedriver 提供端口
-   "host"=>"http://localhost:9515",
+    "host"=>"http://localhost:9515",
     //页面响应等待时间 秒
     "wait"=>15,
 ];
