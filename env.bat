58ead92bfd2f0860f8c699c306b883fa780cb3ca
马鹏飞 <937096063@qq.com>
2020-07-16 12:38:22 +0800

    af

--- env.bat
+++
@@ -1,3 +0,0 @@
-cd C:\\Program\ Files\ \(x86\)\Google\Chrome\Application
-start chromedriver.exe
-cd install_file && java -jar selenium-server-standalone-3.141.59.jar
--- run.bat
+++
@@ -1,5 +0,0 @@
-start env.bat
-php cao.php web1/1
-php cao.php web1/2
-php cao.php web1/3
-php cao.php web1/jiedu
